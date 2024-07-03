dc87530772c6c23a3d088c0a26c74feb47403356
Saif Maamun <sadifmamun@gmail.com>
2024-07-04 02:01:37 +0600

    [Product Interface and model done]

---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import { TProduct } from "./products.interface";
+
+const VariantSchema = new Schema({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const InventorySchema = new Schema({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const ProductSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [VariantSchema], required: true },
+  inventory: { type: InventorySchema, required: true },
+});
+
+const Product = model<TProduct>("product", ProductSchema);
